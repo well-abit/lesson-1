24f5e1854234e1c375d4687a280e8145eef941a2
well-abit <usercomfy14916@gmail.com>
2026-05-24 13:35:55 +0300

    para7

---
+++ para7p4.py
@@ -0,0 +1,15 @@
+def raise_to_degrees(number, max_degrees):
+    i = 0
+    while True:
+    result = number ** i
+        yield result
+    if result > 100 ** 20
+        return
+        i += 1
+
+
+res = raise_to_degrees(2)
+
+#print(list(res))
+for el in res:
+    print(el)
